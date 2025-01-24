e0f418bbc9622a247c873f2e63ad55cd613a14b6
prachigulati <prachigulati569@gmail.com>
2025-01-24 16:01:23 +0530

    login page added

---
+++ app.py
@@ -0,0 +1,41 @@
+# from flask import Flask,render_template
+# app=Flask(__name__)
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+# if __name__ == "__main__":
+#     app.run()
+
+
+
+# from flask import Flask, render_template
+# app=Flask(__name__)
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+# @app.route("/login")
+# def login():
+#     return render_template("login.html")
+# if __name__ == "__main__":
+#     app.run()
+
+
+
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+if __name__ == "__main__":
+    app.run()
